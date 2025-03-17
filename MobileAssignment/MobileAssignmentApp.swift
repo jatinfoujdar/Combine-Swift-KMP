9b69d504824562cd69bdf7cd4bc6dffeb8330db1
Jatin Foujdar <jatin321001@gmail.com>
2025-03-17 18:46:56 +0530

    Initial Commit

---
+++ MobileAssignment/MobileAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAssignmentApp.swift
+//  MobileAssignment
+//
+//  Created by jatin foujdar on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
